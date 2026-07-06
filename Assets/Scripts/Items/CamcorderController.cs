487adb90d5bc05c31dd9a47274e3001e63e478c8
Lee jinhyuk <120433880+jinhyuk0212@users.noreply.github.com>
2026-07-07 02:09:23 +0900

    feat: 카메라 효과 구현
    Global Volume으로 카메라 효과를 구현했다.

--- Assets/Scripts/Items/CamcorderController.cs
+++ Assets/Scripts/Items/CamcorderController.cs
@@ -1,30 +1,71 @@
+using Unity.Cinemachine;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 public class CamcorderController : MonoBehaviour
 {
     [SerializeField] private Animator animator;
     [SerializeField] private GameObject camcorderObject;
+    [SerializeField] private CinemachineCamera playerCamera;
+    [SerializeField] private Volume volume;
+
+    private FilmGrain filmGrain;
+    private Vignette vignette;
+
+    private float normalFOV = 50f;
+    private float camcorderFOV = 40f;
 
     private bool isRaised = false;
 
-    void Update()
+    private void Awake()
     {
-        if (Input.GetMouseButtonDown(1)) // 마우스 오른쪽 버튼 클릭 시
+        volume.profile.TryGet(out filmGrain);
+        volume.profile.TryGet(out vignette);
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(1))
         {
-            isRaised = !isRaised; // 상태 토글
-            animator.SetBool("IsRaised", isRaised); // 애니메이터에 상태 전달
+            isRaised = !isRaised;
+            animator.SetBool("IsRaised", isRaised);
         }
     }
 
-    // Raise 애니메이션 시작 또는 첫 프레임에서 호출
-    public void ShowCamcorder() // 애니메이션 이벤트에서 호출
+    // Raise 애니메이션 시작
+    public void ShowCamcorder()
     {
         camcorderObject.SetActive(true);
     }
 
-    // Lower 애니메이션 마지막 프레임에서 호출
+    // Lower 애니메이션 끝
     public void HideCamcorder()
     {
         camcorderObject.SetActive(false);
+    }
+
+    // Raise 애니메이션 마지막 프레임
+    public void SetCamcorderFOV()
+    {
+        playerCamera.Lens.FieldOfView = camcorderFOV;
+
+        if (filmGrain != null)
+            filmGrain.intensity.value = 0.7f;
+
+        if (vignette != null)
+            vignette.intensity.value = 0.5f;
+    }
+
+    // Lower 애니메이션 마지막 프레임
+    public void SetNormalFOV()
+    {
+        playerCamera.Lens.FieldOfView = normalFOV;
+
+        if (filmGrain != null)
+            filmGrain.intensity.value = 0f;
+
+        if (vignette != null)
+            vignette.intensity.value = 0.2f;
     }
 }
